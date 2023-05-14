ca42767254b7d097dd7a75937ca072e8786eb54d
nguyenhungquang <hungquangnguyen66@gmail.com>
2023-05-15 00:19:35 +0700

    update model loader

--- models/utils.py
+++ models/utils.py
@@ -1,26 +1,38 @@
+import numpy as np
 import types
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import timm
 
 from .robust_vit import VisionTransformer
 from .wrapper import ModelWrapper
+from .noise_resnet import noise_resnet20
+from .vanilla_resnet import vanilla_resnet20
+# from utils_.utils import AverageMeter, RecorderMeter, time_string, convert_secs2time
 
 class RandomDefense(nn.Module):
-    def __init__(self, noise) -> None:
+    def __init__(self, noise, scale=None) -> None:
         super().__init__()
         self.noise = noise
+        self.scale = nn.Parameter(torch.from_numpy(scale))
 
     def forward(self, x):
-        return x + self.noise * torch.randn_like(x)
+        if self.scale is not None:
+            out = x + self.noise * torch.randn_like(x) * self.scale#.to(x)
+        else:
+            out = x + self.noise * torch.randn_like(x)
+        return out
 
-def defense_token(x, defense_type, noise_sigma):
+def defense_token(x, defense_type, noise_sigma, scale=None):
     if defense_type == 'gauss_filter':
         # gauss_x = gauss_filter(x.detach().cpu().numpy(), self.filter_sigma)
         gauss_x = torch.tensor(gauss_x).cuda()
         return gauss_x
     elif defense_type == 'random_noise':
         noise = torch.randn_like(x) * noise_sigma
+        if scale is not None:
+            noise = noise * torch.from_numpy(scale).to(x)
         return x + noise 
     elif defense_type == 'laplace':
         d = torch.distributions.laplace.Laplace(torch.zeros_like(x), noise_sigma * torch.ones_like(x))
@@ -28,7 +40,7 @@ def defense_token(x, defense_type, noise_sigma):
     elif defense_type == 'identical':
         return x
 
-def add_defense(model_name, model, defense_type, def_position, noise, layer_index=-1):
+def add_defense(model_name, model, defense_type, def_position, noise, layer_index=-1, scale=False):
     if isinstance(layer_index, int):
         layer_index = [layer_index]
     if 'resnet' in model_name:
@@ -62,17 +74,37 @@ def add_defense(model_name, model, defense_type, def_position, noise, layer_inde
 
             model.forward_features = types.MethodType(forward_features_new, model)
     if 'vgg' in model_name:
+        # with open(f'stats/{model_name}_cifar10.npy', 'rb') as fw:
+        #     mean = np.load(fw, allow_pickle=True)
+        # mean = mean / mean.mean()
+        # print(layer_index)
+        # with open(f'stats/{model_name}_cifar10_last_std.npy', 'rb') as fw:
+        #     std = np.load(fw, allow_pickle=True).reshape(1, 512, 14, 14)
         if def_position == 'hidden_feature':
+            
             def forward_features_new(self, x):
-                for l in self.features:
+                # print(len([l for l in self.features if 'conv' in l._get_name().lower()]))
+                c = 0
+                for l in self.features: 
                     if 'conv' in l._get_name().lower():
-                        x = defense_token(x, defense_type, noise)
+                        if c in layer_index or layer_index == [-1]:
+                            x = defense_token(x, defense_type, noise)
+                        # if c == 15:
+                        #     x = defense_token(x, defense_type, noise, std)
+                        c += 1
                     x = l(x)
                 return x
             model.forward_features = types.MethodType(forward_features_new, model)
+    if 'vit' in model_name:
+        if def_position == 'hidden_feature':
+            with open(f'stats/{model_name}_cifar10_last_std.npy', 'rb') as fw:
+                std = np.load(fw, allow_pickle=True)
+            model.blocks = nn.Sequential(*sum([[RandomDefense(noise, std if i == 11  and scale else None), b] if i in layer_index or -1 in layer_index else [b] for i, b in enumerate(model.blocks)], []))
     if 'deit' in model_name:
         if def_position == 'hidden_feature':
-            model.blocks = nn.Sequential(*sum([[RandomDefense(noise), b] if i in layer_index or -1 in layer_index else [b] for i, b in enumerate(model.blocks)], []))
+            with open(f'stats/{model_name}_cifar10_last_std.npy', 'rb') as fw:
+                std = np.load(fw, allow_pickle=True)
+            model.blocks = nn.Sequential(*sum([[RandomDefense(noise, std if i == 11  and scale else None), b] if i in layer_index or -1 in layer_index else [b] for i, b in enumerate(model.blocks)], []))
     if 'mixer' in model_name or 'resmlp' in model_name:
         if def_position == 'hidden_feature':
             model.blocks = nn.Sequential(*sum([[RandomDefense(noise), b] if i in layer_index or -1 in layer_index else [b] for i, b in enumerate(model.blocks)], []))
@@ -81,23 +113,54 @@ def add_defense(model_name, model, defense_type, def_position, noise, layer_inde
             for i in range(len(model.network)):
                 if type(model.network[i]).__name__ == 'Sequential':
                     model.network[i] = nn.Sequential(*sum([[RandomDefense(noise), b] if type(b).__name__ == 'PoolFormerBlock' else [b] for b in model.network[i] ], []))
+    if 'vanilla_resnet' in model_name:
+        if def_position == 'hidden_feature':
+            def forward_new(self, x):
+                x = self.conv_1_3x3(x)
+                x = F.relu(self.bn_1(x), inplace=True)
+                x = self.stage_1(x)
+                x = defense_token(x, defense_type, noise)
+                x = self.stage_2(x)
+                x = defense_token(x, defense_type, noise)
+                x = self.stage_3(x)
+                x = defense_token(x, defense_type, noise)
+                x = self.avgpool(x)
+                x = x.view(x.size(0), -1)
+                return self.classifier(x)
+            model.forward = types.MethodType(forward_new, model)
     return model
 
-def create_robust_model(model_name, dataset, n_cls, noise, defense, def_position, device='cpu', layer_index=-1):
-    if 'vit' not in model_name and 'torchvision' not in model_name:
+def create_robust_model(model_name, dataset, n_cls, noise, defense, def_position, device='cpu', layer_index=-1, blackbox=True, scale=False):
+    if 'noise_resnet' in model_name:
+        assert dataset == 'cifar10'
+        base_model = noise_resnet20()
+    elif 'vanilla_resnet' in model_name:
+        assert dataset == 'cifar10'
+        base_model = vanilla_resnet20()
+    elif 'vit' not in model_name and 'torchvision' not in model_name:
         base_model = timm.create_model(model_name, pretrained=False)
     elif 'vit' in model_name:
         if 'small' in model_name:
             kwargs = dict(patch_size=16, embed_dim=384, depth=12, num_heads=6)
         else:
             kwargs = dict()
-        base_model = VisionTransformer(weight_init='skip', num_classes=n_cls, defense_cls=defense, noise_sigma=noise, def_position=def_position, **kwargs)
+        base_model = VisionTransformer(weight_init='skip', num_classes=1000, defense_cls=defense, noise_sigma=noise, def_position=def_position, **kwargs)
         base_model.default_cfg = timm.create_model(model_name).default_cfg
         base_model.layer_index = layer_index
         base_model.set_defense(True)
+
     if dataset == 'cifar10':
+        # breakpoint()
         model_path = model_name.replace('.', '_')
-        base_model.load_state_dict(torch.load(f'pretrain/{model_path}_{dataset}.pth.tar', map_location='cpu')['state_dict'])
+        # path = '../blackbox/CVPR_2019_PNI/code/save/2023-05-04/cifar10_vanilla_resnet20_160_SGD_train_channelwise_3e-4decay/model_best.pth.tar'
+        state_dict = torch.load(f'pretrain/{model_path}_{dataset}.pth.tar', map_location='cpu')['state_dict']
+        # state_dict = torch.load(path, map_location='cpu')['state_dict']
+        if 'noise_resnet' in model_name or model_name == 'vanilla_resnet_adv':
+            # state_tmp = base_model.state_dict()
+            # state_tmp.update(state_dict)
+            state_dict = {k[2:]:v for (k, v) in state_dict.items() if k[0] != '0'}
+            # print(state_dict)
+        base_model.load_state_dict(state_dict)
     elif (dataset == 'imagenet' or dataset == 'imagenet_baseline') and 'torchvision' not in model_name:
         base_model.load_state_dict(timm.create_model(model_name, pretrained=True).state_dict())
             
@@ -105,7 +168,10 @@ def create_robust_model(model_name, dataset, n_cls, noise, defense, def_position
     # base_model.layer_index = layer_index
     # base_model.set_defense(True)
     # base_model = torch.jit.trace(base_model, torch.randn(200, 3, 224, 224))
-    if 'torchvision' in model_name:
+    if 'noise_resnet' in model_name or 'vanilla_resnet' in model_name:
+        mean = [x / 255 for x in [125.3, 123.0, 113.9]]
+        std = [x / 255 for x in [63.0, 62.1, 66.7]]
+    elif 'torchvision' in model_name:
         import torchvision
         base_model = torchvision.models.resnet50(pretrained=True)
         mean = [0.485, 0.456, 0.406]
@@ -114,7 +180,16 @@ def create_robust_model(model_name, dataset, n_cls, noise, defense, def_position
         mean = base_model.default_cfg['mean']
         std = base_model.default_cfg['std']
     base_model.noise_sigma = noise
-    if 'vit' not in model_name and 'torchvision' not in model_name:
-        base_model = add_defense(model_name, base_model, defense, def_position, noise, layer_index)
-    model = ModelWrapper(base_model, num_classes=n_cls, device=device, def_position=def_position, mean=mean, std=std)
-    return model
+    # if 'vit' not in model_name and 'torchvision' not in model_name:
+    if 'torchvision' not in model_name:
+        base_model = add_defense(model_name, base_model, defense, def_position, noise, layer_index, scale=scale)
+    # from foolbox import PyTorchModel
+    # preprocessing = dict(mean=mean, std=std, axis=-3)
+    # base_model.eval()
+    # model = PyTorchModel(base_model, bounds=(0, 1), preprocessing=preprocessing)
+    # return model
+    if blackbox:
+        model = ModelWrapper(base_model, num_classes=n_cls, device=device, def_position=def_position, mean=mean, std=std)
+        return model
+    else:
+        return base_model, mean, std
--- models/wrapper.py
+++ models/wrapper.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 import utils
 import math
@@ -14,36 +15,95 @@ class Normalization(nn.Module):
         x = (x - self.mean) / self.std
         return x
 
-class ModelWrapper:
+class ModelWrapper(nn.Module):
     def __init__(self, model, num_classes=10, def_position=None, device='cpu', mean=None, std=None):
-        
+        super().__init__()
         self.num_classes = num_classes
-        self.batch_size = 128
+        self.batch_size = 64
         self.device = device
         # self.mean = np.reshape([0.485, 0.456, 0.406], [1, 3, 1, 1])
         # self.std = np.reshape([0.229, 0.224, 0.225], [1, 3, 1, 1])
-        self.mean = mean if mean is not None else [0.5, 0.5, 0.5]
-        self.std = std if std is not None else [0.5, 0.5, 0.5]
-        self.mean = np.reshape(self.mean, [1, 3, 1, 1]).astype(np.single)
-        self.std = np.reshape(self.std, [1, 3, 1, 1]).astype(np.single)
+        mean = mean if mean is not None else [0.5, 0.5, 0.5]
+        std = std if std is not None else [0.5, 0.5, 0.5]
+        mean = np.reshape(mean, [1, 3, 1, 1]).astype(np.single)
+        std = np.reshape(std, [1, 3, 1, 1]).astype(np.single)
         # self.mean_torch = torch.tensor(self.mean, device=device, dtype=torch.float32)
         # self.std_torch = torch.tensor(self.std, device=device, dtype=torch.float32)
         self.def_position = def_position
-        self.model = nn.Sequential(Normalization(self.mean, self.std), model)
+        self.model = nn.Sequential(Normalization(mean, std), model)
         self.model.noise_sigma = model.noise_sigma
         self.model.to(device)
         self.model.eval()
+        if def_position == 'aaa_linear':
+            self.attractor_interval = 6
+            self.reverse_step = 1
+            self.calibration_loss_weight = 5
+            self.aaa_iter = 100
+            self.aaa_optimizer_lr = 0.1
+            self.temperature = 1#1.1236
+            self.dev = 0.5
         # self.model.set_defense(True)
         # self.model = torch.jit.trace(self.model, torch.randn(self.batch_size, 3, 224, 224, device=device))
 
-    def __call__(self, x):
+    def aaa_forward(self, x):
+        with torch.no_grad():
+            logits = self.model(x)
+
+        logits_ori = logits.detach()
+        prob_ori = F.softmax(logits_ori / self.temperature, dim=1)
+        prob_max_ori = prob_ori.max(1)[0] ###
+        value, index_ori = torch.topk(logits_ori, k=2, dim=1)
+        #"""
+        mask_first = torch.zeros(logits.shape, device=self.device)
+        mask_first[torch.arange(logits.shape[0]), index_ori[:, 0]] = 1
+        mask_second = torch.zeros(logits.shape, device=self.device)
+        mask_second[torch.arange(logits.shape[0]), index_ori[:, 1]] = 1
+        #"""
+        
+        margin_ori = value[:, 0] - value[:, 1]
+        attractor = ((margin_ori / self.attractor_interval + self.dev).round() - self.dev) * self.attractor_interval
+        target = attractor - self.reverse_step * (margin_ori - attractor)
+        # diff_ori = (margin_ori - target)
+        # real_diff_ori = margin_ori - attractor
+
+        with torch.enable_grad():
+            logits.requires_grad = True
+            optimizer = torch.optim.Adam([logits], lr=self.aaa_optimizer_lr)
+            i = 0 
+            # los_reverse_rate = 0
+            # prd_maintain_rate = 0
+            for i in range(self.aaa_iter):
+            #while i < self.num_iter or los_reverse_rate != 1 or prd_maintain_rate != 1:
+                prob = F.softmax(logits, dim=1)
+                #loss_calibration = (prob.max(1)[0] - prob_max_ori).abs().mean()
+                loss_calibration = ((prob * mask_first).max(1)[0] - prob_max_ori).abs().mean() # better
+                #loss_calibration = (prob - prob_ori).abs().mean()
+
+                value, index = torch.topk(logits, k=2, dim=1) 
+                margin = value[:, 0] - value[:, 1]
+                #margin = (logits * mask_first).max(1)[0] - (logits * mask_second).max(1)[0]
+
+                diff = (margin - target)
+                real_diff = margin - attractor
+                loss_defense = diff.abs().mean()
+                
+                loss = loss_defense + loss_calibration * self.calibration_loss_weight
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+        return logits.detach()
+
+    def forward(self, x):
         # if self.def_position == 'input_noise':
         #     x = x + np.random.normal(scale=self.model.noise_sigma, size=x.shape).astype(np.float32)
         x = x.to(self.device)
         if self.def_position == 'input_noise':
             x = x + self.model.noise_sigma * torch.randn_like(x)
         # x = (x - self.mean_torch) / self.std_torch
-        out = self.model(x)
+        if self.def_position == 'aaa_linear':
+            out = self.aaa_forward(x)
+        else:
+            out = self.model(x)
         if self.def_position == 'logits':
             out = out + self.model.noise_sigma * torch.randn_like(out)
         return out#.cpu()
@@ -78,7 +138,10 @@ class ModelWrapper:
                 # x_batch_torch = (x_batch_torch - self.mean_torch) / self.std_torch
             # for x_batch in loader:
             #     x_batch_torch = x_batch.to(self.device)
-                logits = self.model(x_batch_torch)[:, :self.num_classes]
+                if self.def_position == 'aaa_linear':
+                    logits = self.aaa_forward(x_batch_torch)[:, :self.num_classes]
+                else:
+                    logits = self.model(x_batch_torch)[:, :self.num_classes]
                 if self.def_position == 'logits':
                     logits = logits + self.model.noise_sigma * torch.randn_like(logits)
                 logits = logits.cpu()
